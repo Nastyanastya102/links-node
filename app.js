ccfc28119ad190ef7a57cd6fc7dda84cc1c4270b
StacyKlyashtorna <little.smart.bee@mail.com>
2022-08-08 22:58:21 +0100

    Init commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+// speсial Object Data Modelling for working with mongoDB
+const app = express()
+// Future server
+const PORT = config.get('port') || 5000
+
+// get config from our config directory
+process.on('uncaughtException', (err) => {
+  console.log(err)
+});
+
+app.use(express.json({ extend: true }))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/link', require('./routes/links'))
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    app.listen(PORT, () => {
+      console.log(`App has been started on port ${PORT}`);
+    });
+
+    // server started listen por
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start()
---
+++ routes/links.js
@@ -0,0 +1,62 @@
+const { Router } = require('express')
+
+const config = require('config')
+const { nanoid } = require('nanoid');
+
+const Links = require('../models/links');
+const authMiddleware = require('../middleware/auth.middleware')
+
+const router = Router()
+
+router.post('/generate', authMiddleware, async (req, res) => {
+  try {
+    const baseUrl = config.get('baseUrl')
+    const { from } = req.body
+
+    const code = nanoid()
+    const exist = await Links.findOne({ from })
+
+    if (exist) {
+      res.json({ link: exist })
+    }
+
+    const to = `${baseUrl}/t/${code}`
+
+    const link = new Links({
+      code,
+      to,
+      from,
+      owner: req.user.userId,
+    })
+
+    await link.save()
+
+    res.status(201).json({ link })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something went wrong, please try again' })
+  }
+})
+
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    const links = await Links.find({ owner: req.user.userId })
+
+    res.json(links)
+
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong, please try again' })
+  }
+})
+
+router.get('/:id', authMiddleware, async (req, res) => {
+  try {
+    const link = await Links.findById(req.params.id)
+
+    req.json(link)
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong, please try again' })
+  }
+})
+
+module.exports = router
